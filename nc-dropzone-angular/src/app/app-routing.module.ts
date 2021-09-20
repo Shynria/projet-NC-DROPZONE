5cabac1984011551dec9df74096b888b4dd68dd5
TristanD1 <tristan.dufay@hotmail.fr>
2021-09-20 16:43:35 +0200

    oui

--- nc-dropzone-angular/src/app/app-routing.module.ts
+++ nc-dropzone-angular/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AvionComponent } from './avion/avion.component';
+import { ParachutisteComponent } from './parachutiste/parachutiste.component';
 
 const routes: Routes = [
-  {path: "avion", component: AvionComponent}
+  {path: "avion", component: AvionComponent},
+  {path: "parachutiste", component: ParachutisteComponent}
 ];
 
 @NgModule({
--- nc-dropzone-angular/src/app/parachute/parachute.component.ts
+++ nc-dropzone-angular/src/app/parachute/parachute.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ParachuteService } from '../parachute.service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-parachute',
