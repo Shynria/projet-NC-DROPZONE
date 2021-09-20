36d2c8793197165508e8379a4b36a2d171856eaa
Flavie Clouet <flavie.clouet@gmail.com>
2021-09-20 10:21:56 +0200

    ajout projet boot

---
+++ nc-dropzone-boot/nc-dropzone/src/test/java/fr/formation/ncdropzone/NcDropzoneApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.formation.ncdropzone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NcDropzoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
