5f42ba7d7c01a1a5f941c64595923adfe29710a0
Actal <aimeric.bruno@gmail.com>
2021-09-20 14:27:12 +0200

    ajout navigation basic et variable env

--- nc-dropzone-angular/src/app/app-routing.module.ts
+++ nc-dropzone-angular/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AvionComponent } from './avion/avion.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "avion", component: AvionComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
