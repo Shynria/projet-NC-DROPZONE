eb222d52d2c8c13582370f621f81e4d2a7cfcda8
Flavie Clouet <flavie.clouet@gmail.com>
2021-09-21 14:03:18 +0200

    inscription

--- nc-dropzone-angular/src/app/inscription/inscription.component.ts
+++ nc-dropzone-angular/src/app/inscription/inscription.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Observable, OperatorFunction } from 'rxjs';
+import {debounceTime, distinctUntilChanged, map} from 'rxjs/operators';
+import { ParachutisteService } from '../parachutiste.service';
 
 @Component({
   selector: 'app-inscription',
@@ -6,10 +10,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inscription.component.css']
 })
 export class InscriptionComponent implements OnInit {
+  constructor(private srvParachutiste: ParachutisteService) {}
+  //noms: String = this.srvParachutiste.findByNom;
+  
+  
+  public model: any;
 
-  constructor() { }
-
+  search: OperatorFunction<string, readonly string[]> = (text$: Observable<string>) =>
+    text$.pipe(
+      debounceTime(200),
+      distinctUntilChanged(),
+      map(term => term.length < 2 ? []
+        : this.noms.filter(v => v.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10))
+    )
+  
   ngOnInit(): void {
+    throw new Error('Method not implemented.');
   }
+
 
 }
--- nc-dropzone-boot/src/main/java/fr/formation/dao/IParachutisteDaoJpaRepository.java
+++ nc-dropzone-boot/src/main/java/fr/formation/dao/IParachutisteDaoJpaRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import fr.formation.model.Parachutiste;
 
 public interface IParachutisteDaoJpaRepository extends JpaRepository<Parachutiste, Integer> {
-    
+    public Parachutiste findByNom(String nom);
 }
